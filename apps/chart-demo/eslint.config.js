69eb773cbbc88efeb4c299081dc4e4009367b913
oats7777 <hanmin84200@gmail.com>
2026-06-20 20:42:57 +0900

    chore: reset repository to domain docs

--- apps/chart-demo/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import { createTypeScriptConfig } from '../../eslint.base.js';
-
-export default [
-  ...createTypeScriptConfig({
-    tsconfigRootDir: import.meta.dirname,
-    project: './tsconfig.json',
-  }),
-  { ignores: ['dist/', 'node_modules/', '*.js'] },
-];
